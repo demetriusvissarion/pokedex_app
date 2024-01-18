432b08cc6640f56248d1b4fe87ef10fb816ee0f7
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-18 14:55:08 +0000

    Update fetchPokemon

--- api.js
+++ api.js
@@ -1,8 +1,28 @@
-const fetchPokemon = (name) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => response.json()).then((data) => console.log(data));
-}
-// example call:
-// fetchPokemon('ditto');
-// fetchPokemon('pikachu')
+const fetchPokemon = async (name) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await response.json();
+
+    return {
+        name: data.name,
+        id: data.id,
+        height: data.height,
+        weight: data.weight,
+        types: data.types.map(type => type.type.name)
+    }
+};
 
 module.exports = fetchPokemon;
+
+
+// // in node
+// const fetchPokemon = require('./api')
+// fetchPokemon('charizard').then((pokemon) => console.log(pokemon))
+
+// This should log:
+// {
+//   name: 'charizard',
+//   id: 6,
+//   height: 17,
+//   weight: 905,
+//   types: ['fire', 'flying']
+// }
